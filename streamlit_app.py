b101cbd83a3622afd627393f9ba12cfdc75f66e3
kdmaiora <kdmaiora@syr.edu>
2022-02-20 01:49:59 -0500

    top bar ticker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,25 @@
+from gc import collect
+from matplotlib import image
 import streamlit as st
 import pandas as pd
 import numpy as np
 import csv
+from millify import millify
+from OpenSea import OpenSea, Collection, Event
+import requests
 
-from OpenSea import OpenSea
 
+#page initilization
 st.set_page_config(layout="wide")
+oS = OpenSea()
+headerCollection_1 = oS.get_collection("boredapeyachtclub")
+headerCollection_2 = oS.get_collection("azuki")
+headerCollection_3 = oS.get_collection("meebits")
+headerCollection_4 = oS.get_collection("genesis-creepz")
+headerCollection_5 = oS.get_collection("deadfellaz")
+headerCollection_6 = oS.get_collection("cyberkongz")
+headerCollection_7 = oS.get_collection("candy-hunters")
+headerCollection_8 = oS.get_collection("sorare")
 
 st.title('NFT Project Analysis Dashboard', )
 st.markdown("""---""")
@@ -15,48 +29,46 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-with open('collection_names.csv', newline='') as f:
-    reader = csv.reader(f)
-    collections = list(reader)
-
-oS = OpenSea()
-print("{} collections".format(len(collections)))
-c = []
-for collection in collections:
-    c.append(oS.get_collection(collection))
-collections = c
-for collection in collections:
-    print(collection.oneDayChange)
-collections.sort(key=lambda x: x.oneDayChange)
-for collection in collections:
-    print(collection.oneDayChange)
-
 col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
 
 with col1:
     #delta metric
-    st.metric(label="Bored Ape Yacht Club", value="93.9 ETH", delta="-82.18%")
+    st.image(headerCollection_1.image)
+    st.metric(label=headerCollection_1.name, value=str(headerCollection_1.floorPrice) + " ETH", delta=str(round((100*headerCollection_1.oneDayChange), 2)) + "%")
 with col2:
     #delta metric
-    st.metric(label="Cryptopunks", value="67.2 ETH", delta="68.06%")
+    st.image(headerCollection_2.image)
+    st.metric(label=headerCollection_2.name, value=str(headerCollection_2.floorPrice) + " ETH", delta=str(round((100*headerCollection_2.oneDayChange), 2)) + "%")
 with col3:
     #delta metric
-    st.metric(label="Mutant Ape Yacht Club", value="19.7 ETH", delta="-1.74%")
+    st.image(headerCollection_3.image)
+    st.metric(label=headerCollection_3.name, value=str(headerCollection_3.floorPrice) + " ETH", delta=str(round((100*headerCollection_3.oneDayChange), 2)) + "%")
 with col4:
     #delta metric
-    st.metric(label="These Fucking Nuts", value="93.9 ETH", delta="-82.18%")
+    st.image(headerCollection_4.image)
+    st.metric(label=headerCollection_4.name, value=str(headerCollection_4.floorPrice) + " ETH", delta=str(round((100*headerCollection_4.oneDayChange), 2)) + "%")
 with col5:
     #delta metric
-    st.metric(label="Cryptopunks", value="67.2 ETH", delta="68.06%")
+    st.image(headerCollection_5.image)
+    st.metric(label=headerCollection_5.name, value=str(headerCollection_5.floorPrice) + " ETH", delta=str(round((100*headerCollection_5.oneDayChange), 2)) + "%")
 with col6:
     #delta metric
-    st.metric(label="Mutant Ape Yacht Club", value="19.7 ETH", delta="-1.74%")
+    st.image(headerCollection_6.image)
+    st.metric(label=headerCollection_6.name, value=str(headerCollection_6.floorPrice) + " ETH", delta=str(round((100*headerCollection_6.oneDayChange), 2)) + "%")
 with col7:
     #delta metric
-    st.metric(label="Cryptopunks", value="67.2 ETH", delta="68.06%")
+    st.image(headerCollection_7.image)
+    st.metric(label=headerCollection_7.name, value=str(headerCollection_7.floorPrice) + " ETH", delta=str(round((100*headerCollection_7.oneDayChange), 2)) + "%")
 with col8:
     #delta metric
-    st.metric(label="Mutant Ape Yacht Club", value="19.7 ETH", delta="-1.74%")
+    st.image(headerCollection_8.image)
+    st.metric(label=headerCollection_8.name, value=str(headerCollection_8.floorPrice) + " ETH", delta=str(round((100*headerCollection_8.oneDayChange), 2)) + "%")
+
+collectionDF=pd.read_csv(r'collection_names.csv')
+collection_list=collectionDF.drop_duplicates().values
+
+graphOption = st.selectbox('Select a collection to visualize', collection_list) 
+
 
 @st.cache
 def load_data(nrows):
